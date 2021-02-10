3670f69e8515c2eb09438445eeb4011eb81abf62
d1mac9 <dimbizon@gmail.com>
2021-02-10 18:42:19 +0300

    fix reverse and reversedText

--- src/main/kotlin/reverseFromFile.kt
+++ src/main/kotlin/reverseFromFile.kt
@@ -1,8 +1,12 @@
 import java.io.BufferedReader
 import java.io.File
+import java.util.Scanner
 
 fun main() {
-    val bufferedReader: BufferedReader = File("src/main/resources/file.txt").bufferedReader()
+    val scan = Scanner(System.`in`)
+    println("Введите путь до файла: ")
+    val filePath: String = scan.nextLine()
+    val bufferedReader: BufferedReader = File(filePath).bufferedReader()
     val originalText = bufferedReader.use { it.readText() }
     print("Перевернутая фраза: ")
     print(reverseText(originalText))
