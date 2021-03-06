540a388fd6a9f893454d49eafcc1ca0045aabc56
d1mac9 <dimbizon@gmail.com>
2021-03-06 19:57:13 +0300

    fix throw exception tests

--- src/test/kotlin/TestValidationExpression.kt
+++ src/test/kotlin/TestValidationExpression.kt
@@ -1,26 +1,42 @@
 package src.main.kotlin
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 import validationExpression
-import io.kotest.core.test.TestResult
+import io.kotest.matchers.string.startWith
 
 class TestValidationExpression : ShouldSpec() {
     init {
-//        context("Send incorrect symbols") {
-//            validationExpression("!") shouldBe Exception("Введен некорректный символ: \"!\"") //как парсить эксепшн?
-//        }
-//        context("Send incorrect count of brackets") {
-//            validationExpression("(2+3-5))") shouldBe Exception("Неверное расположение скобки(ок)")
-//        }
-//        context("Send empty expression") {
-//            validationExpression("") shouldBe Exception("Введенное выражение пустое")
-//        }
-//        context("2 dots near") {
-//            validationExpression("..2+20") shouldBe Exception("Две точки подряд")
-//        }
-//        context("2 operators near") {
-//            validationExpression("-/2+20") shouldBe Exception("Два оператора подряд")
-//        }
+        context("Send incorrect symbols") {
+            val exception = shouldThrow<Exception> {
+                validationExpression("!")
+            }
+            exception.message!!.should(startWith("Введен некорректный символ: !"))
+        }
+        context("Send incorrect count of brackets") {
+            val exception = shouldThrow<Exception> {
+                validationExpression("(2+3-5))")
+            }
+            exception.message!!.should(startWith("Неверное расположение скобки(ок)"))
+        }
+        context("Send empty expression") {
+            val exception = shouldThrow<Exception> {
+                validationExpression("")
+            }
+            exception.message!!.should(startWith("Введенное выражение пустое"))
+        }
+        context("2 dots near") {
+            val exception = shouldThrow<Exception> {
+                validationExpression("..2+20")
+            }
+            exception.message!!.should(startWith("Две точки подряд"))
+        }
+        context("2 operators near") {
+            val exception = shouldThrow<Exception> {
+                validationExpression("-/2+20")
+            }
+            exception.message!!.should(startWith("Два оператора подряд"))
+        }
         context("Happy path") {
             validationExpression("(2+3-5*9+(1-3))") shouldBe "(2+3-5*9+(1-3))"
         }
