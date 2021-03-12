ab8a927d26e2fd1b7c91fa3e1f907cc525416fae
d1mac9 <dimbizon@gmail.com>
2021-03-12 18:36:14 +0300

    add bi function

--- src/main/kotlin/htmlWordCounter.kt
+++ src/main/kotlin/htmlWordCounter.kt
@@ -1,25 +1,20 @@
 import org.jsoup.Jsoup
+import java.util.concurrent.ConcurrentHashMap
+import java.util.function.BiFunction
 
 fun main() {
     val map: MutableMap<String, Int> = mutableMapOf()
-    val doc = Jsoup.connect("https://championat.com/").get()
-    val text: String = doc.body().text()
-    val listWords = text.split(" ")
-    println("Текст с html страницы: $text")
-    listWords.sorted()
-    var counter = 1
-    for (word in 0..listWords.size - 2) {
-        if (listWords[word] == listWords[word + 1]) {
-            counter++
-            continue
-        }
-        map[listWords[word]] = counter
-        counter = 1
+//    val link = readLine()
+    val html = Jsoup.connect("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0").get()
+    val text = html.text().toLowerCase()
+    println(text)
+    val prepared = text.replace("""[^а-я a-z]""".toRegex(), "")
+    val listWords = prepared.split(" ")
+    for (word in listWords) {
+        map.compute(word
+        ) { k: String, v: Int? -> if (v == null) 1 else v + 1 }
     }
-    if (listWords[listWords.lastIndex] !in map) {
-        map[listWords[listWords.lastIndex]] = 1
-    }
-    println("Частота использования слов: ${map.toSortedMap()}")
+    println("Частота использования слов: ${map}")
     println("Общее число слов: ${listWords.count()}")
 }
 
